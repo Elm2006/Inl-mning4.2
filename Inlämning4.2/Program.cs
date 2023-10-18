73c37f554a766decce3465ad5808f507f21efbbf
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-10-18 10:39:57 +0200

    Update Program.cs

--- Inlämning4.2/Program.cs
+++ Inlämning4.2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics.SymbolStore;
 
 namespace Inlämning_4.__2
@@ -8,9 +8,15 @@ namespace Inlämning_4.__2
         static void Main(string[] args)
         {
             int highTal = 0;
+            Console.WriteLine("Skriv in ett heltal");
+            int diTal = int.Parse(Console.ReadLine());
+            if (diTal > highTal)
+            {
+                highTal = diTal;
+            }
             while (true)
             {
-                Console.WriteLine("Vill du skriva in ett heltal ja eller nej");
+                Console.WriteLine("Vill du skriva in ett till heltal ja eller nej");
                 string svar = Console.ReadLine();
                 if (svar == "nej")
                 {
@@ -18,7 +24,7 @@ namespace Inlämning_4.__2
                 }
 
                 Console.WriteLine("Skriv in ett heltal");
-                int diTal = int.Parse(Console.ReadLine());
+                diTal = int.Parse(Console.ReadLine());
                 if (diTal > highTal)
                 {
                     highTal = diTal;
