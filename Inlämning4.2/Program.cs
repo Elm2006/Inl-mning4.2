64191bd09bd90eba2fb3f17024e19080d967f23b
isel06002 <isel06002@E-PW0234HX>
2023-10-18 10:16:28 +0200

    Add project files.

---
+++ Inlämning4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace Inlämning_4.__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int highTal = 0;
+            while (true)
+            {
+                Console.WriteLine("Vill du skriva in ett heltal ja eller nej");
+                string svar = Console.ReadLine();
+                if (svar == "nej")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Skriv in ett heltal");
+                int diTal = int.Parse(Console.ReadLine());
+                if (diTal > highTal)
+                {
+                    highTal = diTal;
+                }
+
+            }
+            Console.WriteLine("Det största talet var: "+highTal);
+        }
+    }
+}
